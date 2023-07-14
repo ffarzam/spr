fccdcef6a9483ba94abfa9c7511a6a7e81741784
Farzam <ffarzam_1992@yahoo.com>
2023-07-14 12:29:48 +0330

    Updating Authors views

--- BlogPlatform/Authors/views.py
+++ BlogPlatform/Authors/views.py
@@ -7,10 +7,10 @@ from .models import Author
 
 def author_list(request):
     all_author = Author.objects.all()
-    return render(request, "author_list.html", {"all_author": all_author})
+    return render(request, "Author/author_list.html", {"all_author": all_author})
 
 
 def author_details(request, pk):
     author = Author.objects.get(id=pk)
-    return render(request, "author_details.html", {"author": author})
+    return render(request, "Author/author_details.html", {"author": author})
 
