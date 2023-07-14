392dd11e43041d4eeea1e4e2c90589fbcb8e51a5
Farzam <ffarzam_1992@yahoo.com>
2023-07-14 17:38:34 +0330

    Updating Authors models

--- BlogPlatform/Authors/models.py
+++ BlogPlatform/Authors/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=50)
     bio = models.CharField(max_length=500)
+    image = models.ImageField(upload_to='images/', default="def.jpg")
 
     def __str__(self) -> str:
         return f"{self.name}: {self.bio}"
-
